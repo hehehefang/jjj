17a76627abeb5973bb637b48d27e9251b48e4ac9
Fang <771492072@qq.com>
2021-07-09 23:50:40 +0800

    queue

---
+++ main.js
@@ -0,0 +1,30 @@
+
+console.log("hi");
+const divScreen = document.querySelector("#screen");
+const btnCreateNumber = document.querySelector("#createNumber");
+const btnCallNumber = document.querySelector("#callNumber");
+const spanNewNumber = document.querySelector("#newNumber");
+const spanQueue = document.querySelector("#queue");
+console.log("hi");
+
+let n = 0;
+let queue = [];
+console.log("hi");
+btnCreateNumber.onclick = () => {
+    n += 1;
+    //queue.push(n);
+    queue.push.call(queue, n);
+    spanNewNumber.innerText = n;
+    spanQueue.innerText = JSON.stringify(queue);
+};
+
+btnCallNumber.onclick = () => {
+    if (queue.length === 0) {
+        return;
+    }
+    //const m = queue.shift();
+    const m = queue.shift.call(queue);
+    divScreen.innerText = `请 ${m} 号就餐`;
+    spanQueue.innerText = JSON.stringify(queue);
+
+};
